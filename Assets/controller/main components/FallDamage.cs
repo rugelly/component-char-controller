1c9b06ecd6f6282d77745e4f07bb9f1078ed28cd
rugelly <tfholland1@gmail.com>
2020-01-02 23:16:45 -0700

    air jumps refined, added fall dmg + temp ui bars
    hp bar/shield bar/jump juice bar
    double jump still occasionaly gets super speed possible bug with regular jump forgiveness timer being too short?

---
+++ Assets/controller/main components/FallDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    private Health _health;
+
+    public bool landingFirm {get{return impact >= 5 && impact < 10;}} // reset accel mult to 0 regardless of input direction
+    public bool landingHard {get{return impact >= 10 && impact < 15;}} // reset accel mult to 0, slow motor for a short period
+    public bool landingSplat {get{return impact >= 15;}} // reset accel mult to 0, slow motor, take fall dmg
+    private float impact;
+
+    private void OnEnable()
+    {
+        _health = GetComponent<Health>();
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        float falldmg = impact = col.impulse.magnitude;
+        if (landingSplat)
+        {
+            falldmg -= impact;
+            falldmg *= falldmg;
+            _health.Hurt(falldmg);
+        }
+    }
+}
